c486b4213b038bb5834019e05f8922c48359adef
MahmoudAdelSaleh <mahmoudadelsaleh@outlook.com>
2025-08-03 13:53:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-// تسجيل الـ Service Worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-      console.log('✅ Service Worker مسجل بنجاح:', registration);
-    })
-    .catch(function(error) {
-      console.log('❌ فشل تسجيل Service Worker:', error);
-    });
-}
-
-// منع الرجوع بزر العودة
-history.pushState(null, document.title, location.href);
-window.addEventListener('popstate', function () {
-  history.pushState(null, document.title, location.href);
-});
+.
